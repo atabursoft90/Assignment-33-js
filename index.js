15ac7c738b6b7cd05334dcd45429d0494f9bc6b6
atabursoft90 <129271794+atabursoft90@users.noreply.github.com>
2023-04-01 22:13:30 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+/*JavaScript has 8 Datatypes
+
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object*/ 
+
+
+//let a = 50;
+//let b = 80;
+//let sum = a+b;
+//console.log(sum)
+
+ //var x = 70;
+ //var y = 80;
+ //var sum = x+y;
+ //console.log(sum)
+ /*var x = 70;
+ var y = 80;
+     x-= y;
+console.log(x)*/
+ /*var a = 80;
+ var b  = 8;
+    a*= b;
+console.log(a)*/
+
+/*var a =90;
+var b = 60;
+var sum = a*b;
+console.log(sum)*/
+
+//var myname = "korim";
+//console.log(typeof myname);
+
+ // var  a = 1000;
+ //console.log(typeof a); 
+
+ //let x = true;
+ //console.log(typeof x)
+
+ //let x = BigInt("123456789012345678901234567890");
+  //console.log(typeof x)
+
+  //const a = ["Saab","Volvo","BMW"];
+   //console.log ( typeof a);
+
+  // var num =30;
+  // var num2 =50;
+  // console.log(num*num2);
+
+ //function fun(){
+ //var num =30;
+  //var num2 =90;
+ // console.log(num *num2);
+// }
+// fun()
+// fun()/
+
+
